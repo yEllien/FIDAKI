9d7a7dce1a2da23485ef45be1984eb498aea742e
yEllien <elittsa@gmail.com>
2018-11-01 18:26:42 +0200

    added sum stuffz

---
+++ FIDAKI/sfml/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	RenderWindow win (VideoMode(500,500), "snake" );
+	
+
+
+
+	return 0;
+}
