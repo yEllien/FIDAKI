be3202fd0214d5efea6480126cd6d13627a3afb0
yEllien <elittsa@gmail.com>
2018-11-03 23:47:16 +0200

    added snake

--- FIDAKI/sfml/main.cpp
+++ FIDAKI/sfml/main.cpp
@@ -1,14 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 
+#include "Snake.h"
+
+
 using namespace std;
 using namespace sf;
 
+
+ 
 int main()
 {
-	RenderWindow win (VideoMode(500,500), "snake" );
+	RenderWindow win (VideoMode(500,500), "Snake" );
 	
-
 
 
 	return 0;
